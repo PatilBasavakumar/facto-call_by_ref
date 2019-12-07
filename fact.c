113c917d32fc9abb62e8284fb221240b46a92e63
PatilBasavakumar <39963130+PatilBasavakumar@users.noreply.github.com>
2019-12-07 22:53:28 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+long int fact(int*);
+
+int main(void)
+{
+    int num;
+    long int res;
+    printf("\n\t Enter a number: ");
+    scanf("%d", &num);
+    int n = num;
+    res = fact(&num);
+    printf("\n\t Factorial of %d is %ld\n",n,res);
+
+    return 0;
+}
+
+long int fact(int *n)
+{
+    long int val = 1;
+    while(*n > 1)
+    {
+        val = val*(*n);
+        (*n)--;
+    }
+    return val;
+}
