b2e14b04f4f55528ee80b768d7588db650be8d7d
PatilBasavakumar <39963130+PatilBasavakumar@users.noreply.github.com>
2019-12-08 00:43:47 +0530

    Add files via upload

---
+++ check-endian.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int val=0x41424344;
+    char *pc;
+    pc=&val;
+    printf("\n\t %x is the value stored.",*pc);
+    if(*pc == 68)
+        printf("\n\t it's a Little endian system.\n\n");
+    else
+        printf("\n\t it's a Big endian system.\n\n");
+
+    return 0;
+}
